487d5db25cd38b3be391014c1b94ac14f81c367f
Brian Le <bkylele@gmail.com>
2024-07-28 00:40:28 -0700

    Changed default message

--- config.def.h
+++ config.def.h
@@ -12,18 +12,27 @@ static const char *colorname[NUMCOLS] = {
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message =
+"\x1b[38;2;150;117;249m __________________ \n"
+"\x1b[38;2;117;137;249m < Come back soon! >\n"
+"\x1b[38;2;117;223;249m ------------------ \n"
+"\x1b[38;2;117;249;196m        \\   ^__^\n"
+"\x1b[38;2;117;249;177m         \\  (..)\\_______\n"
+"\x1b[38;2;117;249;196m            (__)\\       )\\/\\\n"
+"\x1b[38;2;117;249;150m             U ||----w |\n"
+"\x1b[38;2;137;249;117m                ||     ||\n"
+;
 
 /* text color */
 static const char * text_color = "#ffffff";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x10";
+static const char * font_name = "-misc-jetbrainsmononl nf-bold-r-normal--0-0-0-0-p-0-koi8-uni";
 
 /*Enable blur*/
 #define BLUR
 /*Set blur radius*/
-static const int blurRadius=5;
+static const int blurRadius=20;
 /*Enable Pixelation*/
 //#define PIXELATION
 /*Set pixelation radius*/
